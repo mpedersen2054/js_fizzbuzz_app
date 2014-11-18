4080bd3b5c5259b34cc7b745339ba5a337d85dc8
Matt Pedersen <mpedersen2054@gmail.com>
2014-11-17 20:46:11 -0600

    add completed js for FizzBuzz app

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
 
 $(function() {
-  
+  var body = $("body"),
+      count = 1,
+      newLine = function () {
+        body.append("<hr />");
+      }
+
+  while (count < 101) {
+    
+    if (count % 5 == 0 && count % 3 == 0) {
+      body.append("FizzBuzz");
+      newLine();
+
+    } else if (count % 5 == 0) {
+      body.append("Buzz");
+      newLine();
+    }
+
+    else if (count % 3 == 0) {
+      body.append("Fizz");
+      newLine();
+    }
+
+    else {
+      body.append(count);
+      newLine();
+    }
+
+    count++;
+
+  }
+
+
 })();
